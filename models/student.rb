b2cead0f47b901752c8633c5bf3e202c0a91a1e0
David Eastcroft <davideastcroft95@gmail.com>
2020-04-01 18:20:38 +0100

    Adds methods for saving student data, displaying all student data and returning an individual student's data.

--- models/student.rb
+++ models/student.rb
@@ -13,4 +13,30 @@ class Student
     @id = student['id'].to_i if student['id']
   end
 
+  def save()
+    sql = "INSERT INTO students (first_name, last_name, house, age)
+           VALUES ($1, $2, $3, $4) RETURNING id"
+    values = [@first_name, @last_name, @house, @age]
+    @id = SQLRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM students"
+    students = SQLRunner.run(sql)
+    return students.map {|student| Student.new(student)}
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM students WHERE id = $1"
+    values = [id]
+    student = SQLRunner.run(sql, values).first()
+    return nil if student == nil
+    return Student.new(student)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM students"
+    SQLRunner.run(sql)
+  end
+
 end
