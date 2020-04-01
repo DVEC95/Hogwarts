91180973dda9cf5d4778953d49a6dec4a54f71d8
David Eastcroft <davideastcroft95@gmail.com>
2020-04-01 22:03:59 +0100

    Changes student house from text to foreign key.

--- console.rb
+++ console.rb
@@ -6,11 +6,38 @@ require_relative("./models/house.rb")
 Student.delete_all()
 House.delete_all()
 
+
+#HOUSE DATA ---------
+house1 = House.new({
+  "name" => "Gryffindor",
+  "logo" => "https://vignette.wikia.nocookie.net/harrypotter/images/b/b1/Gryffindor_ClearBG.png/revision/latest?cb=20190222162949"
+  })
+house1.save()
+
+house2 = House.new({
+  "name" => "Slytherin",
+  "logo" => "https://vignette.wikia.nocookie.net/harrypotter/images/0/00/Slytherin_ClearBG.png/revision/latest?cb=20161020182557"
+  })
+house2.save()
+
+house3 = House.new({
+  "name" => "Ravenclaw",
+  "logo" => "https://vignette.wikia.nocookie.net/harrypotter/images/4/4e/RavenclawCrest.png/revision/latest?cb=20161020182442"
+  })
+house3.save()
+
+house4 = House.new({
+  "name" => "Hufflepuff",
+  "logo" => "https://vignette.wikia.nocookie.net/harrypotter/images/0/06/Hufflepuff_ClearBG.png/revision/latest?cb=20161020182518"
+  })
+house4.save()
+
+
 #STUDENT DATA --------
 student1 = Student.new({
+  "house_id" => house1.id,
   "first_name" => "Harry",
   "last_name" => "Potter",
-  "house" => "Gryffindor",
   "age" => 16
   })
 student1.save()
@@ -18,7 +45,7 @@ student1.save()
 student2 = Student.new({
   "first_name" => "Ron",
   "last_name" => "Weasley",
-  "house" => "Gryffindor",
+  "house_id" => house1.id,
   "age" => 16
   })
 student2.save()
@@ -26,7 +53,7 @@ student2.save()
 student3 = Student.new({
   "first_name" => "Hermione",
   "last_name" => "Granger",
-  "house" => "Gryffindor",
+  "house_id" => house1.id,
   "age" => 17
   })
 student3.save()
@@ -34,7 +61,7 @@ student3.save()
 student4 = Student.new({
   "first_name" => "Draco",
   "last_name" => "Malfoy",
-  "house" => "Slytherin",
+  "house_id" => house2.id,
   "age" => 16
   })
 student4.save()
@@ -42,7 +69,7 @@ student4.save()
 student5 = Student.new({
   "first_name" => "Luna",
   "last_name" => "Lovegood",
-  "house" => "Ravenclaw",
+  "house_id" => house3.id,
   "age" => 15
   })
 student5.save()
@@ -50,35 +77,11 @@ student5.save()
 student6 = Student.new({
   "first_name" => "Newton",
   "last_name" => "Scamander",
-  "house" => "Hufflepuff",
+  "house_id" => house4.id,
   "age" => 99
   })
 student6.save()
 
-#HOUSE DATA ---------
-house1 = House.new({
-  "name" => "Gryffindor",
-  "logo" => "https://vignette.wikia.nocookie.net/harrypotter/images/b/b1/Gryffindor_ClearBG.png/revision/latest?cb=20190222162949",
-  })
-house1.save()
-
-house2 = House.new({
-  "name" => "Slytherin",
-  "logo" => "https://vignette.wikia.nocookie.net/harrypotter/images/0/00/Slytherin_ClearBG.png/revision/latest?cb=20161020182557",
-  })
-house2.save()
-
-house3 = House.new({
-  "name" => "Ravenclaw",
-  "logo" => "https://vignette.wikia.nocookie.net/harrypotter/images/4/4e/RavenclawCrest.png/revision/latest?cb=20161020182442",
-  })
-house3.save()
-
-house4 = House.new({
-  "name" => "Hufflepuff",
-  "logo" => "https://vignette.wikia.nocookie.net/harrypotter/images/0/06/Hufflepuff_ClearBG.png/revision/latest?cb=20161020182518",
-  })
-house4.save()
 
 binding.pry
 nil
